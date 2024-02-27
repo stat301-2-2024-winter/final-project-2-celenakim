52639156596ec410acccf62f00545f5ad0679a6b
celenakim <celenakim2026@u.northwestern.edu>
2024-02-26 22:29:01 -0600

    final push before submitting

--- 04_model_analysis.R
+++ 04_model_analysis.R
@@ -21,9 +21,8 @@ load(here("results/null_fit_recipe1.rda"))
 # load pre-processing/feature engineering/recipe
 load(here("results/recipe1_kitchen_sink.rda"))
 load(here("results/recipe2_kitchen_sink_trees.rda"))
-load(here("results/recipe3_interactions.rda"))
-load(here("results/recipe4_trasnformed_trees.rda"))
-
+load(here("results/recipe3_transformed_interactions.rda"))
+load(here("results/recipe4_transformed_trees.rda"))
 set.seed(301)
 
 
@@ -34,7 +33,7 @@ lm_fit_recipe3 |>
   bind_rows(null_fit_recipe1 |> 
               collect_metrics() |>  
               mutate(model = "null")) |> 
-  knitr::kable()
+  knitr::kable(digits = c(NA, NA, 3, 0, 5, NA))
 
 
 
